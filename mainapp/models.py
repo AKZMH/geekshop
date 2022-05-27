702950e632b749a3c5ac722e27d0e2649b11f90c
Andrey Kuzminykh <a.kzmnh@gmail.com>
2022-05-27 14:17:38 +0300

    первая миграция

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, verbose_name='Название', unique=True)
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.pk}. {self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ('name', )
